003bb6f6f27100af0d711aae2bc142afb9735d67
daya-cd <dayasays89>
2016-08-15 17:18:18 +0100

    added intial file for testing

---
+++ js/adapt-testplugin.js
@@ -0,0 +1,18 @@
+define(["coreViews/componentView", "coreJS/adapt"], function(ComponentView, Adapt) {
+
+	var Testplugin = ComponentView.extend({
+
+
+		preRender: function() {
+		
+            alert("test");
+        },
+
+
+
+    });
+    // Adapt.register('testplugin', Testplugin);
+
+    return Testplugin;
+
+});
